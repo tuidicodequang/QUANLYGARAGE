d662719d40581bec8f0373d8cfcec086870a3b26
phan hung anh <phanhunganh2004@outlook.com>
2023-10-23 17:47:06 +0700

    update Phan Hùng Anh

---
+++ QUANLYGARAGE/NhanVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUANLYGARAGE
+{
+    public partial class NhanVien : Form
+    {
+        public NhanVien()
+        {
+            InitializeComponent();
+        }
+        Modify modify;
+        private void NhanVien_Load(object sender, EventArgs e)
+        {
+            modify = new Modify();
+            try
+            {
+                tblEmployee.DataSource = modify.getNhanVien();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
